eb2fb7ed22fd0f857855c2cc7533121641dfa932
Hokyung (Andy) Lee <techandy42@gmail.com>
2022-10-14 14:01:07 -0500

    expanded on existing test files

--- backend_flask/models_data_output.py
+++ backend_flask/models_data_output.py
@@ -6,21 +6,24 @@ print('len(users): ' + str(len(users)))
 for user in users:
   print(str(user.id) + ': ' + user.__repr__())
   print('checklist ' + str(user.id) + ': '  + str(user.checklist))
+  print('work_days ' + str(user.id) + ': '  + str(user.work_days))
 
 # get all workspaces
 workspaces = Workspace.query.all()
 print('len(workspaces): ' + str(len(workspaces)))
 for workspace in workspaces:
   print(str(workspace.id) + ': ' + workspace.__repr__())
+  print('members ' + str(workspace.id) + ': '  + str(workspace.members))
+  print('admins ' + str(workspace.id) + ': '  + str(workspace.admins))
 
 # get all categories
 categories = Category.query.all()
 print('len(categories): ' + str(len(categories)))
 for category in categories:
-  print(str(category.id) + ': ' + category.__repr__())
+  print(str(category.id) + ': ' + category.__repr__() + ' / workspace_id: ' + str(category.workspace_id))
 
 # get all tasks
 tasks = Task.query.all()
 print('len(tasks): ' + str(len(tasks)))
 for task in tasks:
-  print(str(task.id) + ': ' + task.__repr__())
+  print(str(task.id) + ': ' + task.__repr__() + ' / category_id: ' + str(task.category_id) + ' / user_id: ' + str(task.user_id))
---
+++ backend_flask/models_test_user_data_input.py
@@ -0,0 +1,33 @@
+from app import db
+from models import User
+
+print('Enter the user\'s email address:')
+email = input()
+user = User.query.filter_by(email=email).first()
+
+print('Enter the input type: 0 for checklist, 1 for work_time_range, 2 for sleep_time_range, 3 for work_days')
+input_type = int(input())
+
+if input_type == 0:
+  print('Enter the task ids that will go into the checklist, seperated by commas: ex: 1,2,3,...,n')
+  task_ids_separated_by_commas = input()
+  user.checklist = [int(task_id) for task_id in task_ids_separated_by_commas.split(',') if task_id != '']
+  db.session.commit()
+
+if input_type == 1:
+  print('Enter the work time range: ex: 9:00-17:00')
+  work_time_range = input()
+  user.work_time_range = work_time_range
+  db.session.commit()
+
+if input_type == 2:
+  print('Enter the sleep time range: ex: 9:00-17:00')
+  sleep_time_range = input()
+  user.sleep_time_range = sleep_time_range
+  db.session.commit()
+
+if input_type == 3:
+  print('Enter the work days: ex: 0,1,1,1,1,1,0')
+  work_days = input()
+  user.work_days = [bool(int(work_day)) for work_day in work_days.split(',')]
+  db.session.commit()
