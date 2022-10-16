e32be45517e31441a7ff5c17853109a413dfb1ae
Hokyung (Andy) Lee <techandy42@gmail.com>
2022-10-15 20:11:33 -0500

    added and tested for updating Algo calendar and its time_zone

--- backend_flask/calendar_schedule.py
+++ backend_flask/calendar_schedule.py
@@ -83,13 +83,38 @@ def add_task_time_blocks_to_calendar_and_add_task_ids_to_checklist(id):
   dt_total1 = datetime.now() # DEBUG
 
   user = User.query.get(id)
-  calendar_id = user.calendar_id
   refresh_token = user.refresh_token
 
   creds = Credentials(token=None, refresh_token=refresh_token, token_uri=TOKEN_URI, client_id=CLIENT_ID, client_secret=CLIENT_SECRET)
   service = build('calendar', 'v3', credentials=creds)
   time_zone = service.settings().get(setting='timezone').execute()
   time_zone = time_zone['value']
+
+  calendar_list = service.calendarList().list(
+      maxResults=250,
+      showDeleted=False,
+      showHidden=False,
+    ).execute()
+  calendar_ids = [calendar['id'] for calendar in calendar_list['items']]
+  calendar_ids_time_zones = [{ 'id': calendar['id'], 'time_zone': calendar['timeZone'] } for calendar in calendar_list['items']]
+  if user.calendar_id not in calendar_ids:
+    calendar = {
+      'summary': 'Algo',
+      'timeZone': time_zone,
+    }
+    calendar = service.calendars().insert(body=calendar).execute()
+    user.calendar_id = calendar['id']
+    db.session.commit()
+  else:
+    calendar_time_zone = [calendar['time_zone'] for calendar in calendar_ids_time_zones if calendar['id'] == user.calendar_id][0]
+    print('calendar_time_zone: ', calendar_time_zone) # DEBUG
+    if calendar_time_zone != time_zone:
+      calendar = {
+        'summary': 'Algo',
+        'timeZone': time_zone,
+      }
+      calendar = service.calendars().update(calendarId=user.calendar_id, body=calendar).execute()
+  calendar_id = user.calendar_id
 
   dt1 = datetime.now() # DEBUG
   
@@ -99,8 +124,8 @@ def add_task_time_blocks_to_calendar_and_add_task_ids_to_checklist(id):
   runtime_algorithm = dt2 - dt1 # DEBUG
 
   # DEBUG
-  print('allocatable_tasks_time_ranges:')
-  pprint(allocatable_tasks_time_ranges)
+  # print('allocatable_tasks_time_ranges:')
+  # pprint(allocatable_tasks_time_ranges)
 
   task_ids_and_first_time = []
   for task_id_str in allocatable_tasks_time_ranges['work']:
