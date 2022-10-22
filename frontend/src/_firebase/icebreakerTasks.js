d7286e2befd85c02132306baea8495de7e3a0d37
Hokyung (Andy) Lee <andy.lee@plpsd.net>
2022-10-21 22:59:55 -0400

    initialized git repo

---
+++ frontend/src/_firebase/icebreakerTasks.js
@@ -0,0 +1,34 @@
+export const icebreakerTasks = [
+  {
+    name: "Start your own project! 🚀",
+    date: "",
+    projectId: "welcome",
+    taskId: "icebreaker_1",
+    boardStatus: "TODO",
+    completed: false,
+  },
+  {
+    name: "Schedule tasks 📅 ",
+    date: "",
+    projectId: "welcome",
+    taskId: "icebreaker_2",
+    boardStatus: "TODO",
+    completed: false,
+  },
+  {
+    name: "Get organized anywhere!",
+    date: "",
+    projectId: "welcome",
+    taskId: "icebreaker_3",
+    boardStatus: "TODO",
+    completed: false,
+  },
+  {
+    name: "View projects as lists or boards",
+    date: "",
+    projectId: "welcome",
+    taskId: "icebreaker_4",
+    boardStatus: "TODO",
+    completed: false,
+  },
+];
